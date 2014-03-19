7b391a51d9b760c891a6216a30223f136e555522
Bram de Jong <bram.dejong@samplesumo.com>
2014-03-19 21:15:41 +0100

    added shebang and some better output formatting

--- move_pictures.py
+++ move_pictures.py
@@ -1,22 +1,23 @@
+#! /usr/bin/env python
+
 import exifread # pip install ExifRead
 import datetime
 import glob
 import os
 
+
 for filename in glob.glob("*.JPG"):
     tags = exifread.process_file(open(filename, 'rb'))
     
-    picture_time = str(tags['Image DateTime'])
-    
-    picture_time = datetime.datetime.strptime(picture_time, '%Y:%m:%d %H:%M:%S')
+    picture_time = datetime.datetime.strptime(str(tags['Image DateTime']), '%Y:%m:%d %H:%M:%S')
     
     picture_time = picture_time - datetime.timedelta(hours=5) # move all pictures taken before 5 o'clock into the previous day
     
-    directory = picture_time.strftime("%Y-%m-%d__")
+    directory_name = picture_time.strftime("%Y-%m-%d__")
     
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
         
-    os.rename(filename, os.path.join(directory, filename))
+    os.rename(filename, os.path.join(directory_name, filename))
     
-    print filename, picture_time
+    print filename, "->", directory_name
